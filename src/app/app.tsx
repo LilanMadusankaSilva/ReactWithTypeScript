7c7aba3f6dffcaed88210352112dca90a507dd73
lilans <lilans@embla.asia>
2018-12-12 14:33:42 +0530

    Added file-loader & formatted

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Hello } from "./components/home";
+import { Home } from "./components/home/home";
 declare let module: any;
 
-ReactDOM.render(<Hello compiler="Typescript" framework="React" bundler="Webpack" />,
+ReactDOM.render(<Home compiler="Typescript" framework="React" bundler="Webpack" />,
     document.getElementById("root"));
 
 if (module.hot) {
